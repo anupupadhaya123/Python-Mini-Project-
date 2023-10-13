228a10e2a657e79dee0624ffc930f4a27cf98c0f
Anup Upadhaya <58466653+anupupadhaya123@users.noreply.github.com>
2023-10-13 22:23:12 +0545

    Completed time math mini project

--- time_math.py
+++ time_math.py
@@ -1,8 +1,10 @@
 import random
+import time
 
 OPERATORS = ['+',"-","*"]
 MIN_OPERANDS = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 5
 
 def generate_problem():
   left = random.randint(MIN_OPERANDS,MAX_OPERAND)
@@ -14,5 +16,27 @@ def generate_problem():
   answer = eval(expr)
   return expr, answer
 
-expr, answer = generate_problem()
-print(expr, " = " ,answer)
+# expr, answer = generate_problem()
+# print(expr, " = " ,answer)
+
+wrong = 0
+input("Press any button to start!")
+print("------------------------")
+
+start_time = time.time()
+
+
+for i in range(TOTAL_PROBLEMS):
+  expr, answer = generate_problem()
+  while True:
+    guess = input("Problem $" + str(i+1) + ": " + expr + " = ")
+    if guess == str(answer):
+      break
+    wrong += 1
+
+end_time = time.time()
+total_time = end_time - start_time
+
+print("---------------")
+print(f"You completed in {total_time} sec")
+  
